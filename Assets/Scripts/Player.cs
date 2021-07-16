3d8f7b63ac68468f3d9edd85d98358cca5e0262a
austinplane <austinplane@gmail.com>
2021-07-16 11:50:23 -0700

    Caught up to previous progress in tutorial.
    Added tiles & player sprite.
    Added box colliders to player and ground tile.
    Changed order of ground tile.
    Added Rigidbody2D to player, and turned off Z rotation.
    Created player script with basic horizontal movement.
    Added script to player character and tested.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    
+    
+    
+
+
+    void Awake() {
+	
+    }
+
+
+    void Start() {
+        
+    }
+
+    
+    void Update() {
+
+        var horizontal = Input.GetAxis("Horizontal");
+        var rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
+    }
+}
