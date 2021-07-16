8f167e0ded76ae74b394f6c1fcb1691ec2555a47
austinplane <austinplane@gmail.com>
2021-07-16 14:16:43 -0700

    Lesson 2B
    Added variable speed to player using serialized field

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,26 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+
+    [SerializeField] float _speed = 1;
     
-    
-    
-
-
-    void Awake() {
-	
-    }
-
-
-    void Start() {
-        
-    }
 
     
     void Update() {
 
-        var horizontal = Input.GetAxis("Horizontal");
+        var horizontal = Input.GetAxis("Horizontal") * _speed;
         var rigidbody2D = GetComponent<Rigidbody2D>();
         rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
         Debug.Log($"Velocity = {rigidbody2D.velocity}");
