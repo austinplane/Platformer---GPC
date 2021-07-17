e7d3fb8ca56f2b7f51df4922edef660a1c452667
austinplane <austinplane@gmail.com>
2021-07-16 17:14:44 -0700

    End of Lesson 2E
    Added jump, Jump Force variable in Inspector, and added boxcollider2D to floating platform

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     [SerializeField] float _speed = 1;
-    
+    [SerializeField] float _jumpForce = 200;
 
     
     void Update() {
@@ -20,6 +20,10 @@ public class Player : MonoBehaviour {
         if (horizontal != 0) {
             var spriteRenderer = GetComponent<SpriteRenderer>();
             spriteRenderer.flipX = horizontal < 0;
+        }
+
+        if (Input.GetButtonDown("Fire1")) {
+            rigidbody2D.AddForce(Vector2.up * _jumpForce);
         }
     }
 }
