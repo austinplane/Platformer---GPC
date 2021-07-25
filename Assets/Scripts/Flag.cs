9b9025e1ba2c9cba20e7f02c60c85b3e5ae43d36
austinplane <austinplane@gmail.com>
2021-07-25 14:29:42 -0700

    End of Lesson 6D
    Added new scene and ability to transition when player reaches the flag.

--- Assets/Scripts/Flag.cs
+++ Assets/Scripts/Flag.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Flag : MonoBehaviour {
+
+    [SerializeField] string _sceneName;
 
     private void OnTriggerEnter2D(Collider2D collision) {
 
         var player = collision.GetComponent<Player>();
         if (player == null)
             return;
-
-        //TODO: play flag wave
+                
         var animator = GetComponent<Animator>();
         animator.SetTrigger("Raise");
+
         //TODO: load new level
+        SceneManager.LoadScene(_sceneName);
     }
 
 }
