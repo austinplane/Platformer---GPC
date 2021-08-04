d9ee005764efab87f76236beacb9be8eb9d1f307
austinplane <austinplane@gmail.com>
2021-08-03 20:51:23 -0700

    End of Lesson 8B
    Added a release event for the push button & refactored the push button script.

--- Assets/Scripts/PushButtonSwitch.cs
+++ Assets/Scripts/PushButtonSwitch.cs
@@ -1,22 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class PushButtonSwitch : MonoBehaviour {
     
-    [SerializeField] Sprite _buttonDownSprite;
-    [SerializeField] UnityEvent _onEnter; 
+    [SerializeField] Sprite _pressedSprite;
+    [SerializeField] UnityEvent _onPressed;
+    [SerializeField] UnityEvent _onReleased;
+    
+    Sprite _releasedSprite;
+    SpriteRenderer _spriteRenderer;
 
+    private void Awake() {
+
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _releasedSprite = _spriteRenderer.sprite;
+
+        BecomeReleased();
+    }
     void OnTriggerEnter2D(Collider2D collision) {
 
         var player = collision.GetComponent<Player>();
         if (player == null)
             return;
 
-        var _spriteRenderer = GetComponent<SpriteRenderer>();
-        _spriteRenderer.sprite = _buttonDownSprite;
+        BecomePressed();
+    }
 
-        _onEnter?.Invoke();
+    private void OnTriggerExit2D(Collider2D collision) {
+
+        var player = collision.GetComponent<Player>();
+        if (player == null)
+            return;
+
+        BecomeReleased();
+    }
+
+    private void BecomePressed() {
+        _spriteRenderer.sprite = _pressedSprite;
+        _onPressed?.Invoke();
+    }
+    private void BecomeReleased() {
+        _spriteRenderer.sprite = _releasedSprite;
+        _onReleased?.Invoke();
     }
 }
+
