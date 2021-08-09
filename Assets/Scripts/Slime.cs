06b17c82f21dd962b64274383b31243cbd28606f
austinplane <austinplane@gmail.com>
2021-08-08 21:39:27 -0700

    End of Lesson 9B
    Added Raycast to detect ground edge for slime movement. Still need to clean up code that switches direciton

--- Assets/Scripts/Slime.cs
+++ Assets/Scripts/Slime.cs
@@ -1,16 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Slime : MonoBehaviour {
 
-    Rigidbody2D _rigidbody;
-    
+    [SerializeField] Transform _rightSensor;
+    [SerializeField] Transform _leftSensor;
 
+    SpriteRenderer _spriteRenderer;
+    Rigidbody2D _rigidbody;
+    float _direction = 1;
 
     void Awake() {
 
         _rigidbody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
 
@@ -21,7 +26,17 @@ public class Slime : MonoBehaviour {
     
     void Update() {
 
-        _rigidbody.velocity = Vector2.left;
+        _rigidbody.velocity = new Vector2(_direction, _rigidbody.velocity.y);
+        Debug.DrawRay(_rightSensor.position, Vector2.down * 0.1f, Color.red);
+        
+        var result = Physics2D.Raycast(_rightSensor.position, Vector2.down, 0.1f);
+        if (result.collider == null)
+            TurnAround();
+    }
+
+    private void TurnAround() {
+        _direction *= -1;
+        _spriteRenderer.flipX = !_spriteRenderer.flipX;
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
