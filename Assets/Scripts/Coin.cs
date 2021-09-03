939123fe2e5f9c5e8278b5a55c0c43a4e0b4c46d
austinplane <austinplane@gmail.com>
2021-09-03 10:58:23 -0700

    End of Lesson 12B
    Added UI element that displays number of coins collected

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour {
     
-    static int _coinsCollected;
+    public static int CoinsCollected;
 
     void OnTriggerEnter2D(Collider2D collider) {
 
@@ -14,8 +14,8 @@ public class Coin : MonoBehaviour {
             return;
 
         gameObject.SetActive(false);
-        _coinsCollected++;
-        Debug.Log(_coinsCollected);
+        CoinsCollected++;
+        Debug.Log(CoinsCollected);
     }
 
 }
