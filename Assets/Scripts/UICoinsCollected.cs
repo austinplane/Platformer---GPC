d3dd32d6f5f9c92e0f1f9b0b7edde3ef763d3762
austinplane <austinplane@gmail.com>
2021-09-03 13:19:40 -0700

    End of Lesson 13B
    Fixed reference to TMP component in code.

--- Assets/Scripts/UICoinsCollected.cs
+++ Assets/Scripts/UICoinsCollected.cs
@@ -1,22 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class UICoinsCollected : MonoBehaviour {
 
 
-    Text _text;
+    TMP_Text _text;
 
 
     void Start() {
 
-        _text = GetComponent<Text>();
+        _text = GetComponent<TMP_Text>();
     }
 
     
     void Update() {
 
-        _text.text = $"Coins Collected: {Coin.CoinsCollected}";
+        _text.SetText($"Coins Collected: {Coin.CoinsCollected}");
     }
 }
