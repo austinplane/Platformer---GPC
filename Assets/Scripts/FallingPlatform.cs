b99c1c8a5f00b31d06415828731b851a76a0e3d8
austinplane <austinplane@gmail.com>
2021-09-03 20:40:29 -0700

    End of Lesson 13E
    Added wiggling behavior to falling platform.

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -8,6 +8,12 @@ public class FallingPlatform : MonoBehaviour {
     public bool PlayerInside;
     
     HashSet<Player> _playersInTrigger = new HashSet<Player>();
+    Vector3 _initialPosition;
+
+    void Start() {
+
+        _initialPosition = transform.position;
+    }
 
     void OnTriggerEnter2D(Collider2D collider) {
 
@@ -18,7 +24,9 @@ public class FallingPlatform : MonoBehaviour {
         _playersInTrigger.Add(player);
 
         PlayerInside = true;
-        StartCoroutine(WiggleAndFall());
+
+        if (_playersInTrigger.Count == 1)
+            StartCoroutine(WiggleAndFall());
     }
 
     void OnTriggerExit2D(Collider2D collider) {
@@ -29,8 +37,10 @@ public class FallingPlatform : MonoBehaviour {
 
         _playersInTrigger.Remove(player);
 
-        if (_playersInTrigger.Count == 0)
+        if (_playersInTrigger.Count == 0) {
             PlayerInside = false;
+            StopCoroutine(WiggleAndFall());
+        }
     }
 
     IEnumerator WiggleAndFall() {
@@ -38,6 +48,19 @@ public class FallingPlatform : MonoBehaviour {
         Debug.Log("Waiting to Wiggle");
         yield return new WaitForSeconds(0.25f);
         Debug.Log("Wiggling");
+        
+        float wiggleTimer = 0f;
+
+        while (wiggleTimer < 1f) {
+
+            float randomX = UnityEngine.Random.Range(-0.05f, 0.05f);
+            float randomY = UnityEngine.Random.Range(-0.05f, 0.05f);
+            transform.position = _initialPosition + new Vector3(randomX, randomY);
+            float randomDelay = UnityEngine.Random.Range(0.005f, 0.01f);
+            yield return new WaitForSeconds(randomDelay);
+            wiggleTimer += randomDelay;
+        }
+
         yield return new WaitForSeconds(1f);
         Debug.Log("Falling");
         yield return new WaitForSeconds(3f);
