67c81613c462e6c7d0f3f6972b508d1dd2e014d1
austinplane <austinplane@gmail.com>
2021-09-05 19:33:36 -0700

    End of Lesson 14B
    Added keylock + behavior

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+
+    [SerializeField] KeyLock _keyLock;
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+
+        var player = collider.GetComponent<Player>();
+
+        if (player != null) {
+
+            transform.SetParent(player.transform);
+            transform.localPosition = Vector3.up;
+        }
+
+        var keylock = collider.GetComponent<KeyLock>();
+        if (keylock != null && keylock == _keyLock) {
+
+            keylock.Unlock();
+            Destroy(gameObject);
+        }
+
+    }
+}
