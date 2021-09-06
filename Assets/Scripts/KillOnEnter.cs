67b802cda95e89fb98ad14570dbf45f2ce0d4e51
austinplane <austinplane@gmail.com>
2021-09-06 10:36:16 -0700

    End of Lesson 14E
    Added fireball particle effect and killonenter behavior.

--- Assets/Scripts/KillOnEnter.cs
+++ Assets/Scripts/KillOnEnter.cs
@@ -1,9 +1,19 @@
 ﻿using UnityEngine;
 
 public class KillOnEnter : MonoBehaviour {
-        void OnTriggerEnter2D(Collider2D collision) {
+    
+    void OnTriggerEnter2D(Collider2D collision) {
 
         var player = collision.GetComponent<Player>();
+        if (player != null) {
+            player.ResetToStart();
+        }
+
+    }
+
+    private void OnParticleCollision(GameObject other) {
+
+        var player = other.GetComponent<Player>();
         if (player != null) {
             player.ResetToStart();
         }
