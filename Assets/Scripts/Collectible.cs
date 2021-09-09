3e87328d924242a38f50d26c46213e8cb84ee15b
austinplane <austinplane@gmail.com>
2021-09-08 20:27:52 -0700

    End of Lesson 15A
    Added initial collection system

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Collectible : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+
+        var player = collider.GetComponent<Player>();
+        
+        if (player == null)
+            return;
+
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Collector.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collector : MonoBehaviour {
+
+    [SerializeField] Collectible[] _collectiblesToCollect;    
+
+
+    void Awake() {
+	
+    }
+
+
+    void Start() {
+        
+    }
+
+    
+    void Update() {
+        
+    }
+}
