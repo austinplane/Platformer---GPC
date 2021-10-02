8383dc258264cb15175810cf881cc5a68bfdf823
austinplane <austinplane@gmail.com>
2021-10-01 22:59:47 -0700

    End of Lesson 17B
    Added item box

---
+++ Assets/Scripts/ItemBox.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBox : MonoBehaviour {
+    
+    [SerializeField] Sprite _usedSprite;
+    [SerializeField] GameObject _item;
+    [SerializeField] Vector2 _itemLaunchVelocity;
+    
+    bool _itemUsed;
+
+    void Start() {
+
+        _item.SetActive(false);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+
+        if (_itemUsed)
+            return;
+
+        var player = collision.collider.GetComponent<Player>();
+
+        if (player == null)
+            return;
+
+        if (collision.contacts[0].normal.y > 0) {
+
+            GetComponent<SpriteRenderer>().sprite = _usedSprite;
+
+            _itemUsed = true;
+            _item.SetActive(true);
+            
+            var itemRigidBody = _item.GetComponent<Rigidbody2D>();
+            if (itemRigidBody != null) {
+
+                itemRigidBody.velocity = _itemLaunchVelocity;
+            }
+        }
+    }
+}
