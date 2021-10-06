15731925f6399ce5dc50b092b37d7841f55058ae
austinplane <austinplane@gmail.com>
2021-10-05 20:32:09 -0700

    End of Lesson 17D
    Refactored ItemBox class

--- Assets/Scripts/ItemBox.cs
+++ Assets/Scripts/ItemBox.cs
@@ -6,36 +6,26 @@ public class ItemBox : HittableFromBelow {
     
     [SerializeField] GameObject _item;
     [SerializeField] Vector2 _itemLaunchVelocity;
-    
-    bool _itemUsed;
+
+    bool _itemUsed = false;
+    protected override bool CanUse => !_itemUsed;
 
     void Start() {
 
-        _item.SetActive(false);
+        _item?.SetActive(false);
     }
 
-    void OnCollisionEnter2D(Collision2D collision) {
 
-        if (_itemUsed)
-            return;
+    protected override void Use() {
+        
+        base.Use();
+        _itemUsed = true;
+        _item?.SetActive(true);
 
-        var player = collision.collider.GetComponent<Player>();
+        var itemRigidBody = _item?.GetComponent<Rigidbody2D>();
+        if (itemRigidBody != null) {
 
-        if (player == null)
-            return;
-
-        if (collision.contacts[0].normal.y > 0) {
-
-            GetComponent<SpriteRenderer>().sprite = _usedSprite;
-
-            _itemUsed = true;
-            _item.SetActive(true);
-            
-            var itemRigidBody = _item.GetComponent<Rigidbody2D>();
-            if (itemRigidBody != null) {
-
-                itemRigidBody.velocity = _itemLaunchVelocity;
-            }
+            itemRigidBody.velocity = _itemLaunchVelocity;
         }
     }
 }
